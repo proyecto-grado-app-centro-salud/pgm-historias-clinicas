a4ed4889e2151f19c38cf6f0e53179cf981c9136
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-05-19 21:59:36 -0400

    [Add] peticion obtener listado de historias clinicas de pacientes

--- src/main/java/com/example/microserviciohistoriasclinicas/controller/HistoriaClinicaController.java
+++ src/main/java/com/example/microserviciohistoriasclinicas/controller/HistoriaClinicaController.java
@@ -1,7 +1,11 @@
 package com.example.microserviciohistoriasclinicas.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +23,9 @@ public class HistoriaClinicaController {
     public @ResponseBody String registrarReserva(@RequestBody HistoriaClinicaEntity nuevo){
         historiaClinicaRepositoryJPA.save(nuevo);
         return "Ok";
+    }
+    @GetMapping("/{idPaciente}")
+    public @ResponseBody List<HistoriaClinicaEntity> controllerMethod(@PathVariable int idPaciente) {
+        return historiaClinicaRepositoryJPA.findByIdPaciente(idPaciente);
     }
 }
