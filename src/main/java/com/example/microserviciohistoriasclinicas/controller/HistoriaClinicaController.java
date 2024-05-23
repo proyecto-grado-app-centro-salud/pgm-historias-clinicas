dfc37f0ebae5372f813ff79d158eb31829af07b8
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-05-23 19:35:08 -0400

    [Add] obtener historias clnicas y actualizar historias clinicas

--- src/main/java/com/example/microserviciohistoriasclinicas/controller/HistoriaClinicaController.java
+++ src/main/java/com/example/microserviciohistoriasclinicas/controller/HistoriaClinicaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,9 +28,35 @@ public class HistoriaClinicaController {
         historiaClinicaRepositoryJPA.save(nuevo);
         return "Ok";
     }
+    @PutMapping("/{id}")
+    public @ResponseBody String actualizarHistoriaClinica(@PathVariable Integer id, @RequestBody HistoriaClinicaEntity actualizada) {
+        return historiaClinicaRepositoryJPA.findById(id)
+                .map(historiaClinica -> {
+                    historiaClinica.setAmnesis(actualizada.getAmnesis());
+                    historiaClinica.setAntecedentesFamiliares(actualizada.getAntecedentesFamiliares());
+                    historiaClinica.setAntecedentesGinecoobstetricos(actualizada.getAntecedentesGinecoobstetricos());
+                    historiaClinica.setAntecedentesNoPatologicos(actualizada.getAntecedentesNoPatologicos());
+                    historiaClinica.setAntecedentesPersonales(actualizada.getAntecedentesPersonales());
+                    historiaClinica.setDiagnosticoPresuntivo(actualizada.getDiagnosticoPresuntivo());
+                    historiaClinica.setDiagnosticosDiferenciales(actualizada.getDiagnosticosDiferenciales());
+                    historiaClinica.setExamenFisico(actualizada.getExamenFisico());
+                    historiaClinica.setExamenFisicoEspecial(actualizada.getExamenFisicoEspecial());
+                    historiaClinica.setPropuestaBasicaDeConducta(actualizada.getPropuestaBasicaDeConducta());
+                    historiaClinica.setTratamiento(actualizada.getTratamiento());
+                    historiaClinicaRepositoryJPA.save(historiaClinica);
+                    return "Historia clínica actualizada con éxito";
+                })
+                .orElseGet(() -> {
+                    return "Historia clínica con ID " + id + " no encontrada";
+                });
+    }
     @GetMapping("/{idPaciente}")
     public @ResponseBody List<HistoriaClinicaEntity> controllerMethod(@PathVariable int idPaciente) {
         return historiaClinicaRepositoryJPA.findByIdPaciente(idPaciente);
+    }
+    @GetMapping()
+    public @ResponseBody List<HistoriaClinicaEntity> obtenerTodasHistoriasClinicas() {
+        return historiaClinicaRepositoryJPA.findAll();
     }
     @GetMapping("/info-container")
     public @ResponseBody String obtenerInformacionContenedor() {
