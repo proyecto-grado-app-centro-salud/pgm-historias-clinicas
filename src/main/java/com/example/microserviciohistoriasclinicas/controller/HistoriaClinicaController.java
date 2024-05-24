17b06e4a28dcf081351fa9cbf87c6809af72659e
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-05-23 20:03:48 -0400

    [Add] agregando obtener detalle de historia clinica por id

--- src/main/java/com/example/microserviciohistoriasclinicas/controller/HistoriaClinicaController.java
+++ src/main/java/com/example/microserviciohistoriasclinicas/controller/HistoriaClinicaController.java
@@ -3,6 +3,7 @@ package com.example.microserviciohistoriasclinicas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.microserviciohistoriasclinicas.model.HistoriaClinicaEntity;
 import com.example.microserviciohistoriasclinicas.repository.HistoriaClinicaRepositoryJPA;
@@ -50,9 +52,14 @@ public class HistoriaClinicaController {
                     return "Historia clínica con ID " + id + " no encontrada";
                 });
     }
-    @GetMapping("/{idPaciente}")
+    @GetMapping("/paciente/{idPaciente}")
     public @ResponseBody List<HistoriaClinicaEntity> controllerMethod(@PathVariable int idPaciente) {
         return historiaClinicaRepositoryJPA.findByIdPaciente(idPaciente);
+    }
+    @GetMapping("/{idHistoriaClinica}")
+    public @ResponseBody HistoriaClinicaEntity obtenerDetalleHistoriaClinica(@PathVariable int idHistoriaClinica) {
+        return historiaClinicaRepositoryJPA.findById(idHistoriaClinica)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Historia clínica con ID " + idHistoriaClinica + " no encontrada"));
     }
     @GetMapping()
     public @ResponseBody List<HistoriaClinicaEntity> obtenerTodasHistoriasClinicas() {
