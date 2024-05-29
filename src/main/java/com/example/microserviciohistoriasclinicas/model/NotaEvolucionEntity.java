d1f5a4ef98b0b3d6e34939e1d14a3e7fce572f67
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-05-29 17:55:43 -0400

    [Fix] arreglando peticiones de notas de evolucion

--- src/main/java/com/example/microserviciohistoriasclinicas/model/NotaEvolucionEntity.java
+++ src/main/java/com/example/microserviciohistoriasclinicas/model/NotaEvolucionEntity.java
@@ -2,6 +2,10 @@ package com.example.microserviciohistoriasclinicas.model;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,12 +15,22 @@ import lombok.Setter;
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "notas_evolucion")
 public class NotaEvolucionEntity {
+    @Id
+    @Column(name = "id_nota_evolucion")
     private int idNotaEvolucion;
+    @Column(name = "id_historia_clinica")
     private int idHistoriaClinica;
+    @Column(name = "cambios_paciente_resultados_tratamiento")
     private String cambiosPacienteResultadosTratamiento;
+    @Column(name = "id_medico")
     private int idMedico;
+    @Column(name = "created_at")
     private Date createdAt;
+    @Column(name = "updated_at")
     private Date updatedAt;
+    @Column(name = "deleted_at")
     private Date deletedAt;
 }
--- src/main/java/com/example/microserviciohistoriasclinicas/repository/NotaEvolucionRepository.java
+++ src/main/java/com/example/microserviciohistoriasclinicas/repository/NotaEvolucionRepository.java
@@ -8,21 +8,21 @@ import org.springframework.data.jpa.repository.Query;
 import com.example.microserviciohistoriasclinicas.model.NotaEvolucionEntity;
 
 public interface NotaEvolucionRepository extends JpaRepository<NotaEvolucionEntity, Integer> {
-    @Query(value="SELECT id_nota_evolucion,hc.id_historia_clinica,cambios_paciente_resultados_tratamiento,id_medico,ne.created_at,ne.updated_at,ne.deleted_at,CONCAT(p.nombres,' ',p.apellido_paterno,' ',p.apellido_materno),p.ci "+
+    @Query(value="SELECT id_nota_evolucion,hc.id_historia_clinica,cambios_paciente_resultados_tratamiento,ne.id_medico,ne.created_at,ne.updated_at,ne.deleted_at,CONCAT(p.nombres,' ',p.apellido_paterno,' ',p.apellido_materno),p.ci "+
     "FROM notas_evolucion ne "+
     "INNER JOIN historias_clinicas hc ON hc.id_historia_clinica = ne.id_historia_clinica "+
     "INNER JOIN pacientes p ON hc.id_paciente = p.id_paciente "+
     "INNER JOIN medicos m ON m.id_medico = ne.id_medico ",
     nativeQuery=true)
     List<Object[]> obtenerNotasEvolucion();
-    @Query(value="SELECT id_nota_evolucion,hc.id_historia_clinica,cambios_paciente_resultados_tratamiento,id_medico,ne.created_at,ne.updated_at,ne.deleted_at,CONCAT(p.nombres,' ',p.apellido_paterno,' ',p.apellido_materno),p.ci "+
+    @Query(value="SELECT id_nota_evolucion,hc.id_historia_clinica,cambios_paciente_resultados_tratamiento,ne.id_medico,ne.created_at,ne.updated_at,ne.deleted_at,CONCAT(p.nombres,' ',p.apellido_paterno,' ',p.apellido_materno),p.ci "+
     "FROM notas_evolucion ne "+
     "INNER JOIN historias_clinicas hc ON hc.id_historia_clinica = ne.id_historia_clinica "+
     "INNER JOIN pacientes p ON hc.id_paciente = p.id_paciente "+
     "INNER JOIN medicos m ON m.id_medico = ne.id_medico "+
     "WHERE p.id_paciente=?1",nativeQuery=true)
     List<Object[]> obtenerNotasEvolucionPaciente(int idPaciente);
-    @Query(value="SELECT id_nota_evolucion,hc.id_historia_clinica,cambios_paciente_resultados_tratamiento,id_medico,ne.created_at,ne.updated_at,ne.deleted_at,CONCAT(p.nombres,' ',p.apellido_paterno,' ',p.apellido_materno),p.ci "+
+    @Query(value="SELECT id_nota_evolucion,hc.id_historia_clinica,cambios_paciente_resultados_tratamiento,ne.id_medico,ne.created_at,ne.updated_at,ne.deleted_at,CONCAT(p.nombres,' ',p.apellido_paterno,' ',p.apellido_materno),p.ci "+
     "FROM notas_evolucion ne "+
     "INNER JOIN historias_clinicas hc ON hc.id_historia_clinica = ne.id_historia_clinica "+
     "INNER JOIN pacientes p ON hc.id_paciente = p.id_paciente "+
