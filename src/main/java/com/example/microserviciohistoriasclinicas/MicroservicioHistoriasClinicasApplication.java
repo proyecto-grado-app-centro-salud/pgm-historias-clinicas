6fa155580effeda2320ef1360ffb58644e8888ee
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-11-24 07:55:00 -0400

    [Fix] definiendo metodos permitidos en peticiones

--- src/main/java/com/example/microserviciohistoriasclinicas/MicroservicioHistoriasClinicasApplication.java
+++ src/main/java/com/example/microserviciohistoriasclinicas/MicroservicioHistoriasClinicasApplication.java
@@ -3,7 +3,8 @@ package com.example.microserviciohistoriasclinicas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+import org.springframework.web.bind.annotation.RequestMethod;
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,RequestMethod.OPTIONS})
 @SpringBootApplication
 public class MicroservicioHistoriasClinicasApplication {
 
